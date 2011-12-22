f16fc73863aac7a1411a00cbf4cf80b2c7c01b02
Julian Langschaedel <meta.rb@gmail.com>
2011-12-22 16:12:18 +0100

    refactor script runner

--- lib/bitcoin/script.rb
+++ lib/bitcoin/script.rb
@@ -2,17 +2,17 @@ require 'bitcoin'
 
 module Bitcoin
   class Script
-    OP_TRUE        = 81
     OP_1           = 81
-    OP_FALSE       = 0
+    OP_TRUE        = 81
     OP_0           = 0
-    OP_2_16        = (82..96).to_a
+    OP_FALSE       = 0
     OP_PUSHDATA1   = 76
     OP_PUSHDATA2   = 77
     OP_PUSHDATA4   = 78
     OP_DUP         = 118
     OP_HASH160     = 169
     OP_EQUAL       = 135
+    OP_VERIFY      = 105
     OP_EQUALVERIFY = 136
     OP_CHECKSIG    = 172
     OP_CHECKSIGVERIFY      = 173
@@ -32,7 +32,12 @@ module Bitcoin
     OP_SHA1         = 167
     OP_RIPEMD160    = 166
 
-    attr_reader :raw, :chunks
+    OPCODES = Hash[*constants.grep(/^OP_/).map{|i| [const_get(i), i.to_s] }.flatten]
+    OPCODES[0] = "0"
+
+    OP_2_16 = (82..96).to_a
+
+    attr_reader :raw, :chunks, :debug
 
     # create a new script. +bytes+ is typically input_script + output_script
     def initialize(bytes, offset=0)
@@ -76,30 +81,7 @@ module Bitcoin
         case i
         when Fixnum
           case i
-          when OP_DUP;         "OP_DUP"
-          when OP_SHA256;      "OP_SHA256"
-          when OP_HASH160;     "OP_HASH160"
-          when OP_HASH256;     "OP_HASH256"
-          when OP_SHA256;      "OP_SHA256"
-          when OP_SHA1;        "OP_SHA1"
-          when OP_RIPEMD160;   "OP_RIPEMD160"
-          when OP_CHECKSIG;    "OP_CHECKSIG"
-          when OP_EQUAL;       "OP_EQUAL"
-          when OP_EQUALVERIFY; "OP_EQUALVERIFY"
-          when OP_CHECKSIGVERIFY;      "OP_CHECKSIGVERIFY"
-          when OP_CHECKMULTISIG;       "OP_CHECKMULTISIG"
-          when OP_CHECKMULTISIGVERIFY; "OP_CHECKMULTISIGVERIFY"
-          when OP_TOALTSTACK;          "OP_TOALTSTACK"
-          when OP_FROMALTSTACK;        "OP_FROMALTSTACK"
-          when OP_TUCK;                "OP_TUCK"
-          when OP_SWAP;                "OP_SWAP"
-          when OP_BOOLAND;             "OP_BOOLAND"
-          when OP_ADD;                 "OP_ADD"
-          when OP_SUB;                 "OP_SUB"
-          when OP_GREATERTHANOREQUAL;  "OP_GREATERTHANOREQUAL"
-          when OP_DROP;                "OP_DROP"
-          when OP_0;                   "0"
-          when OP_1;                   "1"
+          when *OPCODES.keys;          OPCODES[i]
           when *OP_2_16;               (OP_2_16.index(i)+2).to_s
           else "(opcode #{i})"
           end
@@ -118,34 +100,9 @@ module Bitcoin
     def self.binary_from_string(script_string)
       script_string.split(" ").map{|i|
         case i
-          when "OP_DUP";         OP_DUP
-          when "OP_SHA256";      OP_SHA256
-          when "OP_HASH160";     OP_HASH160
-          when "OP_HASH256";     OP_HASH256
-          when "OP_SHA256";      OP_SHA256
-          when "OP_SHA1";        OP_SHA1
-          when "OP_RIPEMD160";   OP_RIPEMD160
-          when "OP_CHECKSIG";    OP_CHECKSIG
-          when "OP_EQUAL";       OP_EQUAL
-          when "OP_EQUALVERIFY"; OP_EQUALVERIFY
-          when "OP_CHECKSIGVERIFY";      OP_CHECKSIGVERIFY
-          when "OP_CHECKMULTISIG";       OP_CHECKMULTISIG
-          when "OP_CHECKMULTISIGVERIFY"; OP_CHECKMULTISIGVERIFY
-          when "OP_TOALTSTACK";          OP_TOALTSTACK
-          when "OP_FROMALTSTACK";        OP_FROMALTSTACK
-          when "OP_TUCK";                OP_TUCK
-          when "OP_SWAP";                OP_SWAP
-          when "OP_BOOLAND";             OP_BOOLAND
-          when "OP_ADD";                 OP_ADD
-          when "OP_SUB";                 OP_SUB
-          when "OP_GREATERTHANOREQUAL";  OP_GREATERTHANOREQUAL
-          when "OP_DROP";                OP_DROP
-          when "0";                      OP_0
-          when "OP_FALSE";               OP_0
-          when "1";                      OP_1
-          when "OP_TRUE";                OP_1
+          when *OPCODES.values;          OPCODES.find{|k,v| v == i }.first
           when /^([2-9]$|1[0-7])$/;      OP_2_16[$1.to_i-2]
-          when /\(opcode (\d+)\)/; $1.to_i
+          when /\(opcode (\d+)\)/;       $1.to_i
           else 
             data = [i].pack("H*")
             size = data.bytesize
@@ -167,99 +124,168 @@ module Bitcoin
       }.join
     end
 
+
+    # Duplicates the top stack item.
+    def op_dup
+      @stack << @stack[-1].dup
+    end
+
+    # The input is hashed using SHA-256.
+    def op_sha256
+      buf = @stack.pop
+      @stack << Digest::SHA256.digest(buf)
+    end
+
+    # The input is hashed using SHA-1.
+    def op_sha1
+      buf = @stack.pop
+      @stack << Digest::SHA1.digest(buf)
+    end
+
+    # The input is hashed twice: first with SHA-256 and then with RIPEMD-160.
+    def op_hash160
+      buf = @stack.pop
+      @stack << Digest::RMD160.digest(Digest::SHA256.digest(buf))
+    end
+
+    # The input is hashed using RIPEMD-160.
+    def op_ripemd160
+      buf = @stack.pop
+      @stack << Digest::RMD160.digest(buf)
+    end
+
+    # The input is hashed two times with SHA-256.
+    def op_hash256
+      buf = @stack.pop
+      @stack << Digest::SHA256.digest(Digest::SHA256.digest(buf))
+    end
+
+    # Puts the input onto the top of the alt stack. Removes it from the main stack.
+    def op_toaltstack
+      @stack_alt << @stack.pop(1)
+    end
+
+    # Puts the input onto the top of the main stack. Removes it from the alt stack.
+    def op_fromaltstack
+      @stack << @stack_alt.pop(1)
+    end
+
+    # The item at the top of the stack is copied and inserted before the second-to-top item.
+    def op_tuck
+      @stack[-2..-1] = [ @stack[-1], *@stack[-2..-1] ]
+    end
+
+    # The top two items on the stack are swapped.
+    def op_swap
+      @stack[-2..-1] = @stack[-2..-1].reverse
+    end
+
+    # If both a and b are not 0, the output is 1. Otherwise 0.
+    def op_booland
+      a, b = @stack.pop(2)
+      @stack << ![a,b].any?{|n| n == 0 } ? 1 : 0
+    end
+
+    # a is added to b.
+    def op_add
+      a, b = @stack.pop(2).reverse
+      @stack << a + b
+    end
+
+    # b is subtracted from a.
+    def op_sub
+      a, b = @stack.pop(2).reverse
+      @stack << a - b
+    end
+
+    # Returns 1 if a is greater than or equal to b, 0 otherwise.
+    def op_greaterthanorequal
+      a, b = @stack.pop(2).reverse
+      @stack << (a >= b) ? 1 : 0
+    end
+
+    # Removes the top stack item.
+    def op_drop
+      @stack.pop(1)
+    end
+
+    # Returns 1 if the inputs are exactly equal, 0 otherwise.
+    def op_equal
+      a, b = @stack.pop(2).reverse
+      @stack << (a == b ? 1 : 0)
+    end
+
+    # Marks transaction as invalid if top stack value is not true. True is removed, but false is not.
+    def op_verify
+      res = @stack.pop
+      if res != 1
+        @stack << res
+        @script_invalid = true # raise 'transaction invalid' ?
+      end
+    end
+
+    # Same as OP_EQUAL, but runs OP_VERIFY afterward.
+    def op_equalverify
+      op_equal; op_verify
+    end
+
+    # An empty array of bytes is pushed onto the stack.
+    def op_0
+      @stack << "" # []
+    end
+
+    # The number 1 is pushed onto the stack.
+    def op_1
+      @stack << 1
+    end
+
+    OPCODES_METHOD = Hash[*instance_methods.grep(/^op_/).map{|m|
+      [ (OPCODES.find{|k,v| v == m.to_s.upcase }.first rescue nil), m ]
+    }.flatten]
+    OPCODES_METHOD[0]  = :op_0
+
+
     # run the script. +check_callback+ is called for OP_CHECKSIG operations
-    def run(debug = [], &check_callback)
+    def run(&check_callback)
+      @debug = []
       @chunks.each{|chunk|
-        debug << @stack.map{|i| i.unpack("H*")}
+        @debug << @stack.map{|i| i.unpack("H*")}
         case chunk
         when Fixnum
           case chunk
-          when OP_DUP
-            debug << "OP_DUP"
-            @stack << @stack[-1].dup
-          when OP_SHA256
-            # The input is hashed using SHA-256.
-            debug << "OP_SHA256"
-            buf = @stack.pop
-            @stack << Digest::SHA256.digest(buf)
-          when OP_SHA1
-            # The input is hashed using SHA-1.
-            debug << "OP_SHA1"
-            buf = @stack.pop
-            @stack << Digest::SHA1.digest(buf)
-          when OP_RIPEMD160
-            # The input is hashed using RIPEMD-160.
-            debug << "OP_RIPEMD160"
-            buf = @stack.pop
-            @stack << Digest::RMD160.digest(buf)
-          when OP_HASH160
-            # The input is hashed twice: first with SHA-256 and then with RIPEMD-160.
-            debug << "OP_HASH160"
-            buf = @stack.pop
-            @stack << Digest::RMD160.digest(Digest::SHA256.digest(buf))
-          when OP_HASH256
-            # The input is hashed two times with SHA-256.
-            debug << "OP_HASH256"
-            buf = @stack.pop
-            @stack << Digest::SHA256.digest(Digest::SHA256.digest(buf))
+
+          when *OPCODES_METHOD.keys
+            m = OPCODES_METHOD[chunk]
+            @debug << m.to_s.upcase
+            send(m) # invoke opcode method
+
           when OP_CHECKSIG
-            debug << "OP_CHECKSIG"
+            @debug << "OP_CHECKSIG"
             op_checksig(check_callback)
-          when OP_EQUALVERIFY
-            debug << "OP_EQUALVERIFY"
-            a, b = @stack.pop(2).reverse
-            return :EQUALVERIFY_FAILED if a != b
-          when OP_CHECKSIGVERIFY
-            raise "opcode OP_CHECKSIGVERIFY not implemented yet."
-          when OP_CHECKMULTISIG
-            raise "opcode OP_CHECKMULTISIG not implemented yet."
-          when OP_CHECKMULTISIGVERIFY
-            raise "opcode OP_CHECKMULTISIGVERIFY not implemented yet."
-          when OP_TOALTSTACK
-            # Puts the input onto the top of the alt stack. Removes it from the main stack.
-            @stack_alt << @stack.pop(1)
-          when OP_FROMALTSTACK
-            # Puts the input onto the top of the main stack. Removes it from the alt stack.
-            @stack << @stack_alt.pop(1)
-          when OP_TUCK
-            # The item at the top of the stack is copied and inserted before the second-to-top item.
-            @stack[-2..-1] = [ @stack[-1], *@stack[-2..-1] ]
-          when OP_SWAP
-            @stack[-2..-1] = @stack[-2..-1].reverse
-          when OP_BOOLAND
-            # If both a and b are not 0, the output is 1. Otherwise 0.
-            a, b = @stack.pop(2)
-            @stack << ![a,b].any?{|n| n == 0 } ? 1 : 0
-          when OP_ADD
-            a, b = @stack.pop(2).reverse
-            @stack << a + b
-          when OP_SUB
-            a, b = @stack.pop(2).reverse
-            @stack << a - b
-          when OP_GREATERTHANOREQUAL
-            a, b = @stack.pop(2).reverse
-            @stack << (a >= b) ? 1 : 0
-          when OP_DROP
-            # Removes the top stack item.
-            @stack.pop(1)
-          when OP_0
-            # An empty array of bytes is pushed onto the stack.
-            @stack << "" # []
-          when OP_1
-            @stack << 1
+
           when OP_2_16
             @stack << OP_2_16.index(chunk)+2
-          else raise "opcode #{i} unkown or not implemented"
+
+          else
+            name = OPCODES[chunk] || chunk
+            raise "opcode #{name} unkown or not implemented"
           end
         when String
-          debug << "PUSH DATA #{chunk.unpack("H*")[0]}"
+          @debug << "PUSH DATA #{chunk.unpack("H*")[0]}"
           @stack << chunk
         end
       }
-      debug << @stack.map{|i| i.unpack("H*") rescue i}
-      debug << "RESULT"
-      @stack.pop == true
+      @debug << @stack.map{|i| i.unpack("H*") rescue i}
+
+      if @script_invalid
+        @stack << 0
+        @debug << "INVALID TRANSACTION"
+      end
+
+      @debug << "RESULT"
+      @stack.pop == 1
     end
+
 
     # do a CHECKSIG operation on the current stack,
     # asking +check_callback+ to do the actual signature verification.
@@ -272,10 +298,10 @@ module Bitcoin
       sig       = sig_and_hash_type[0...-1]
 
       if check_callback == nil # for tests
-        @stack << true
+        @stack << 1
       else # real signature check callback
         @stack <<
-          (check_callback.call(pubkey, sig, hash_type) == true)
+          ((check_callback.call(pubkey, sig, hash_type) == true) ? 1 : 0)
       end
     end
 
@@ -312,7 +338,7 @@ module Bitcoin
     # get the hash160 for this script (in standard address scripts)
     def get_hash160
       return @chunks[2..-3][0].unpack("H*")[0]  if is_hash160?
-      return Bitcoin.hash160(get_pubkey)  if is_pubkey?
+      return Bitcoin.hash160(get_pubkey)        if is_pubkey?
     end
 
     # get the address for the script hash160 (in standard address scripts)
