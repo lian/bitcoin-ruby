769e04b225a3fd17d5056b35df30735a3dd0f54e
Marius Hanne <marius.hanne@sourceagency.org>
2012-01-22 00:17:22 +0100

    config categories

--- lib/bitcoin/config.rb
+++ lib/bitcoin/config.rb
@@ -4,20 +4,27 @@ module Bitcoin
 
     CONFIG_PATHS = "./bitcoin-ruby.yml:~/.bitcoin-ruby.yml:/etc/bitcoin-ruby.yml"
 
-    def self.load(options, paths = CONFIG_PATHS)
+    def self.load(options, category = nil, paths = CONFIG_PATHS)
       paths.split(":").reverse.each do |path|
         path.sub!("~", ENV["HOME"])
         next  unless File.exist?(path)
-        options = load_file(options, path)
+        options = load_file(options, path, category)
       end
       options
     end
 
-    def self.load_file(options, file)
-      YAML::load_file(file).map{|k,v| options[k.to_sym] = v }
+    def self.load_file(options, file, category = nil)
+      categories = YAML::load_file(file)
+      options = load_category(options, categories["all"])
+      options = load_category(options, categories[category.to_s])  if category
       options
     end
 
+    def self.load_category(options, category)
+      return options  unless category
+      category.map{|k,v| options[k.to_sym] = v }
+      options
+    end
   end
 
 end
