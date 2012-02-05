c33947f895f384c923aa7a3311bd02b391e5530e
Julian Langschaedel <meta.rb@gmail.com>
2012-02-05 16:18:07 +0100

    move Bitcoin::OpenSSL_EC specs

--- lib/bitcoin/ffi/openssl.rb
+++ lib/bitcoin/ffi/openssl.rb
@@ -1,10 +1,10 @@
-# autoload when you need to re-generate a public_key from its private_key.
+# autoload when you need to re-generate a public_key from only its private_key.
 # ported from: https://github.com/sipa/bitcoin/blob/2d40fe4da9ea82af4b652b691a4185431d6e47a8/key.h
 
 begin
   require 'ffi'
 rescue LoadError
-  puts "Cannot load 'ffi' - install with `gem install ffi`"
+  puts "Cannot load 'ffi' for OpenSSL_EC.regenerate_key. - install with `gem install ffi`"
   exit 1
 end
 
@@ -39,6 +39,9 @@ module OpenSSL_EC
   attach_function :i2d_ECPrivateKey, [:pointer, :pointer], :int
 
 
+  # resolve public from private key, using ffi and libssl.so
+  # example:
+  #   keypair = Bitcoin.generate_key; Bitcoin::OpenSSL_EC.regenerate_key(keypair.first) == keypair
   def self.regenerate_key(private_key)
     private_key = [private_key].pack("H*") if private_key.bytesize >= (32*2)
 
@@ -90,33 +93,4 @@ module OpenSSL_EC
     @ssl_loaded = true
   end
 end
-end
-
-
-if $0 == __FILE__
-
-  privkey = ["56e28a425a7b588973b5db962a09b1aca7bdc4a7268cdd671d03c52a997255dc"].pack("H*")
-  pubkey =  ["04324c6ebdcf079db6c9209a6b715b955622561262cde13a8a1df8ae0ef030eaa1552e31f8be90c385e27883a9d82780283d19507d7fa2e1e71a1d11bc3a52caf3"].pack("H*")
-  p [privkey.bytesize, pubkey.bytesize]
-  p [privkey, pubkey].map{|i| i.unpack("H*")[0] }
-
-  p Bitcoin::OpenSSL_EC.regenerate_key(privkey)
-  p Bitcoin::OpenSSL_EC.regenerate_key(privkey) == [privkey, pubkey].map{|i| i.unpack("H*")[0] }
-  puts
-
-  p [
-    ["b51386f8275d49d8d30287d7b1afa805790bdd1fe8b13d22d25928c67ea55d02", "0470305ae5278a22499980286d9c513861d89e7b7317c8b891c554d5c8fdd256b03daa0340be4104f8c84cfa98f0da8f16567fcdd3a00fd993adbbe91695671a56"],
-    ["d8ebece51adc5fb99dd6994bcb8fa1221d01576fd76af9134ab36f8d4698b55c", "047503421850d3a6eecb7c9de33b367c4d3f96a34ff257ad0c34e234e29f3672525c6b4353ce6fdc9de3f885fdea798982e2252e610065dbdb62cd8cab1fe45822"],
-    ["c95c79fb0cc1fe47b384751df0627be40bbe481ec94eeafeb6dc40e94c40de43", "04b746ca07e718c7ca26d4eeec037492777f48bb5c750e972621698f699f530535c0ffa96dad581102d0471add88e691af85955d1fd42f68506f8092fddfe0c47a"],
-    ["5b61f807cc938b0fd3ec8f6006737d0002ceca09f296204138c4459de8a856f6", "0487357bf30c13d47d955666f42f87690cfd18be96cc74cda711da74bf76b08ebc6055aba30680e6288df14bda68c781cbf71eaad096c3639e9724c5e26f3acf54"]
-  ].map{|key|
-    privkey, pubkey = *key.map{|i| [i].pack("H*") }
-    p key
-    p Bitcoin::OpenSSL_EC.regenerate_key(privkey)
-    Bitcoin::OpenSSL_EC.regenerate_key(privkey) == key
-  }.all?
-
-  #puts
-  #3.times{ p k=Bitcoin.generate_key; p Bitcoin::OpenSSL_EC.regenerate_key(k.first) }
-
 end
--- spec/bitcoin/key_spec.rb
+++ spec/bitcoin/key_spec.rb
@@ -146,3 +146,34 @@ describe "Bitcoin::KeyGenerator" do
   end
 
 end
+
+
+begin
+  describe "Bitcoin::OpenSSL_EC" do
+    Bitcoin::OpenSSL_EC
+
+    it 'resolves public from private key' do
+      privkey = ["56e28a425a7b588973b5db962a09b1aca7bdc4a7268cdd671d03c52a997255dc"].pack("H*")
+      pubkey =  ["04324c6ebdcf079db6c9209a6b715b955622561262cde13a8a1df8ae0ef030eaa1552e31f8be90c385e27883a9d82780283d19507d7fa2e1e71a1d11bc3a52caf3"].pack("H*")
+
+      Bitcoin::OpenSSL_EC.regenerate_key(privkey).should == [privkey, pubkey].map{|i| i.unpack("H*")[0] }
+
+      [
+        ["b51386f8275d49d8d30287d7b1afa805790bdd1fe8b13d22d25928c67ea55d02", "0470305ae5278a22499980286d9c513861d89e7b7317c8b891c554d5c8fdd256b03daa0340be4104f8c84cfa98f0da8f16567fcdd3a00fd993adbbe91695671a56"],
+        ["d8ebece51adc5fb99dd6994bcb8fa1221d01576fd76af9134ab36f8d4698b55c", "047503421850d3a6eecb7c9de33b367c4d3f96a34ff257ad0c34e234e29f3672525c6b4353ce6fdc9de3f885fdea798982e2252e610065dbdb62cd8cab1fe45822"],
+        ["c95c79fb0cc1fe47b384751df0627be40bbe481ec94eeafeb6dc40e94c40de43", "04b746ca07e718c7ca26d4eeec037492777f48bb5c750e972621698f699f530535c0ffa96dad581102d0471add88e691af85955d1fd42f68506f8092fddfe0c47a"],
+        ["5b61f807cc938b0fd3ec8f6006737d0002ceca09f296204138c4459de8a856f6", "0487357bf30c13d47d955666f42f87690cfd18be96cc74cda711da74bf76b08ebc6055aba30680e6288df14bda68c781cbf71eaad096c3639e9724c5e26f3acf54"]
+      ].each{|key|
+        privkey, pubkey = [ key.first ].pack("H*")
+        Bitcoin::OpenSSL_EC.regenerate_key(privkey).should == key
+      }
+
+      5.times{
+        keypair = Bitcoin.generate_key;
+        Bitcoin::OpenSSL_EC.regenerate_key(keypair.first).should == keypair
+      }
+    end
+
+  end
+rescue LoadError
+end
