fb29a50cc5516d41cc77979eb2def2f1d4699f8e
Julian Langschaedel <meta.rb@gmail.com>
2012-02-20 01:10:05 +0100

    update version spec

--- spec/bitcoin/protocol/version_spec.rb
+++ spec/bitcoin/protocol/version_spec.rb
@@ -4,7 +4,7 @@ describe 'Bitcoin::Protocol::Parser (version)' do
 
   it 'parses version' do
     pkt = [
-      "f9 be b4 d9 76 65 72 73 69 6f 6e 00 00 00 00 00 55 00 00 00 40 9c 00 00 01 00 00 00 00 00 00 00 10 42 c9 4e 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 7f 00 00 01 04 d2 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 7f 00 00 01 47 9d 4b b8 bb 21 ae d7 f0 71 00 fa 00 00 00"
+      "f9 be b4 d9 76 65 72 73 69 6f 6e 00 00 00 00 00 55 00 00 00 a4 c2 08 cb 40 9c 00 00 01 00 00 00 00 00 00 00 10 42 c9 4e 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 7f 00 00 01 04 d2 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ff ff 7f 00 00 01 47 9d 4b b8 bb 21 ae d7 f0 71 00 fa 00 00 00"
       .split(" ").join].pack("H*")
 
     class Version_Handler < Bitcoin::Protocol::Handler
