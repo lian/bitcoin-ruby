dec22a912c13d6f91bb6578f98965f882889ab68
Marius Hanne <marius.hanne@sourceagency.org>
2012-05-09 19:29:58 +0200

    documentation for Bitcoin::Builder

--- lib/bitcoin/builder.rb
+++ lib/bitcoin/builder.rb
@@ -1,197 +1,255 @@
 module Bitcoin
 
+  # Optional DSL to help create blocks and transactions.
+  #
+  # see also BlockBuilder, TxBuilder, TxInBuilder, TxOutBuilder, ScriptBuilder
   module Builder
 
+    # create a Bitcoin::Protocol::Block matching the given +target+.
+    # see BlockBuilder for details.
     def blk(target = "00".ljust(32, 'f'), &block)
       c = BlockBuilder.new
       c.instance_eval &block
       c.block(target)
     end
 
+    # create a Bitcoin::Protocol::Tx.
+    # see TxBuilder for details.
     def tx &block
       c = TxBuilder.new
       c.instance_eval &block
       c.tx
     end
 
-  end
+    # DSL to create a Bitcoin::Protocol::Block used by Builder#blk.
+    #  block = blk("00".ljust(32, 'f')) do
+    #    prev_block "\x00"*32
+    #    tx do
+    #      input { coinbase }
+    #      output do
+    #        value 5000000000;
+    #        script { type :address; recipient Bitcoin::Key.generate.addr }
+    #      end
+    #    end
+    #  end
+    class BlockBuilder
 
+      def initialize
+        @block = Bitcoin::P::Block.new(nil)
+      end
 
-  class BlockBuilder
+      # specify block version. this is usually not necessary. defaults to 1.
+      def version v
+        @version = v
+      end
 
-    def initialize
-      @block = Bitcoin::P::Block.new(nil)
-    end
+      # set the hash of the previous block.
+      def prev_block hash
+        @prev_block = hash
+      end
 
-    def version v
-      @version = v
-    end
+      # add transactions to the block (see TxBuilder).
+      def tx &block
+        c = TxBuilder.new
+        c.instance_eval &block
+        @block.tx << c.tx
+      end
 
-    def prev_block hash
-      @prev_block = hash
-    end
+      # create the block according to values specified via DSL.
+      def block target
+        @block.ver = @version || 1
+        @block.prev_block = [@prev_block].pack("H*").reverse
+        @block.mrkl_root = @mrkl_root
+        @block.time = Time.now.to_i
+        @block.nonce = 0
+        @block.mrkl_root = [Bitcoin.hash_mrkl_tree(@block.tx.map {|t|
+              t.hash }).last].pack("H*")
+        find_hash(target)
+        Bitcoin::P::Block.new(@block.to_payload)
+      end
 
-    def tx &block
-      c = TxBuilder.new
-      c.instance_eval &block
-      @block.tx << c.tx
-    end
+      private
 
-    def block target
-      @block.ver = @version || 1
-      @block.prev_block = [@prev_block].pack("H*").reverse
-      @block.mrkl_root = @mrkl_root
-      @block.time = Time.now.to_i
-      @block.nonce = 0
-      @block.mrkl_root = [Bitcoin.hash_mrkl_tree(@block.tx.map {|t|
-            t.hash }).last].pack("H*")
-      find_hash(target)
-      Bitcoin::P::Block.new(@block.to_payload)
-    end
-
-    def find_hash target
-      @block.bits = Bitcoin.encode_compact_bits(target)
-      t = Time.now
-      @block.recalc_block_hash
-      until @block.hash < target
-        @block.nonce += 1
+      # increment nonce/time to find a block hash matching the +target+.
+      def find_hash target
+        @block.bits = Bitcoin.encode_compact_bits(target)
+        t = Time.now
         @block.recalc_block_hash
-        if @block.nonce == 100000
-          if t
-            tt = 1 / ((Time.now - t) / 100000) / 1000
-            print "\r%.2f khash/s" % tt
+        until @block.hash < target
+          @block.nonce += 1
+          @block.recalc_block_hash
+          if @block.nonce == 100000
+            if t
+              tt = 1 / ((Time.now - t) / 100000) / 1000
+              print "\r%.2f khash/s" % tt
+            end
+            t = Time.now
+            @block.time = Time.now.to_i
+            @block.nonce = 0
+            $stdout.flush
           end
-          t = Time.now
-          @block.time = Time.now.to_i
-          @block.nonce = 0
-          $stdout.flush
         end
       end
+
     end
 
-  end
+    # DSL to create Bitcoin::Protocol::Tx used by Builder#tx.
+    # tx = tx do
+    #   input do
+    #     prev_out prev_tx  # previous transaction
+    #     prev_out_index 0  # index of previous output
+    #     signature_key key # Bitcoin::Key used to sign the input
+    #   end
+    #   output do
+    #     value 12345 # 0.00012345 BTC
+    #     script { type :address; recipient key.addr }
+    #   end
+    # end
+    class TxBuilder
 
-  class TxBuilder
+      def initialize
+        @tx = Bitcoin::P::Tx.new(nil)
+        @tx.ver, @tx.lock_time = 1, 0
+        @ins, @outs = [], []
+      end
 
-    def initialize
-      @tx = Bitcoin::P::Tx.new(nil)
-      @tx.ver, @tx.lock_time = 1, 0
-      @ins, @outs = [], []
-    end
+      # specify tx version. this is usually not necessary. defaults to 1.
+      def version n
+        @tx.ver = n
+      end
 
-    def version n
-      @tx.ver = n
-    end
+      # specify tx lock_time. this is usually not necessary. defaults to 0.
+      def lock_time n
+        @tx.lock_time = n
+      end
 
-    def lock_time n
-      @tx.lock_time = n
-    end
+      # add an input to the transaction (see TxInBuilder).
+      def input &block
+        c = TxInBuilder.new
+        c.instance_eval &block
+        @ins << c
+      end
 
-    def input &block
-      c = TxInBuilder.new
-      c.instance_eval &block
-      @ins << c
-    end
+      # add an output to the transaction (see TxOutBuilder).
+      def output &block
+        c = TxOutBuilder.new
+        c.instance_eval &block
+        @outs << c
+      end
 
-    def output &block
-      c = TxOutBuilder.new
-      c.instance_eval &block
-      @outs << c
-    end
-
-    def tx
-      @ins.each {|i| @tx.add_in(i.txin) }
-      @outs.each {|o| @tx.add_out(o.txout) }
-      @ins.each_with_index do |inc, i|
-        if @tx.in[i].coinbase?
-          script_sig = [inc.coinbase_data].pack("H*")
+      # create the transaction according to values specified via DSL and sign inputs.
+      def tx
+        @ins.each {|i| @tx.add_in(i.txin) }
+        @outs.each {|o| @tx.add_out(o.txout) }
+        @ins.each_with_index do |inc, i|
+          if @tx.in[i].coinbase?
+            script_sig = [inc.coinbase_data].pack("H*")
+            @tx.in[i].script_sig_length = script_sig.bytesize
+            @tx.in[i].script_sig = script_sig
+            next
+          end
+          prev_tx = inc.instance_variable_get(:@prev_out)
+          sig_hash = @tx.signature_hash_for_input(i, prev_tx)
+          sig = inc.key.sign(sig_hash)
+          script_sig = Bitcoin::Script.to_signature_pubkey_script(sig, [inc.key.pub].pack("H*"))
           @tx.in[i].script_sig_length = script_sig.bytesize
           @tx.in[i].script_sig = script_sig
-          next
+          raise "Signature error"  unless @tx.verify_input_signature(i, prev_tx)
         end
-        prev_tx = inc.instance_variable_get(:@prev_out)
-        sig_hash = @tx.signature_hash_for_input(i, prev_tx)
-        sig = inc.key.sign(sig_hash)
-        script_sig = Bitcoin::Script.to_signature_pubkey_script(sig, [inc.key.pub].pack("H*"))
-        @tx.in[i].script_sig_length = script_sig.bytesize
-        @tx.in[i].script_sig = script_sig
-        raise "Signature error"  unless @tx.verify_input_signature(i, prev_tx)
+        Bitcoin::P::Tx.new(@tx.to_payload)
       end
-      Bitcoin::P::Tx.new(@tx.to_payload)
     end
+
+    # create a Bitcoin::Protocol::TxIn used by TxBuilder#input.
+    #
+    # inputs can be either 'coinbase', in which case they only need to specify #coinbase,
+    # or they have to define a #prev_out, #prev_out_index and #signature key.
+    class TxInBuilder
+      attr_reader :key, :coinbase_data
+
+      def initialize
+        @txin = Bitcoin::P::TxIn.new
+      end
+
+      # previous transaction that contains the output we want to use.
+      def prev_out tx
+        @prev_out = tx
+      end
+
+      # index of the output in the #prev_out transaction.
+      def prev_out_index i
+        @prev_out_index = i
+      end
+
+      # specify sequence. this is usually not needed.
+      def sequence s
+        @sequence = s
+      end
+
+      # Bitcoin::Key used to sign the signature_hash for the input.
+      # see Bitcoin::Script.signature_hash_for_input and Bitcoin::Key.sign.
+      def signature_key key
+        @key = key
+      end
+
+      # specify that this is a coinbase input. optionally set +data+.
+      def coinbase data = nil
+        @coinbase_data = data || OpenSSL::Random.random_bytes(32)
+        @prev_out = nil
+        @prev_out_index = 4294967295
+      end
+
+      # create the txin according to values specified via DSL
+      def txin
+        @txin.prev_out = (@prev_out ? [@prev_out.hash].pack("H*").reverse : "\x00"*32)
+        @txin.prev_out_index = @prev_out_index
+        @txin.sequence = @sequence || "\xff\xff\xff\xff"
+        @txin
+      end
+    end
+
+    # create a Bitcoin::Script used by TxOutBuilder#script.
+    class ScriptBuilder
+      attr_reader :script
+
+      def initialize
+        @type = nil
+        @script = nil
+      end
+
+      # script type (:pubkey, :address, :hash160, :multisig).
+      def type type
+        @type = type.to_sym
+      end
+
+      # recipient(s) of the script.
+      # depending on the #type, either an address, hash160 pubkey, etc.
+      def recipient data
+        @script = Bitcoin::Script.send("to_#{@type}_script", data)
+      end
+    end
+
+    # create a Bitcoin::Protocol::TxOut used by TxBuilder#output.
+    class TxOutBuilder
+      attr_reader :txout
+
+      def initialize
+        @txout = Bitcoin::P::TxOut.new
+      end
+
+      # set output value (in base units / "satoshis")
+      def value value
+        @txout.value = value
+      end
+
+      # add a script to the output (see ScriptBuilder).
+      def script &block
+        c = ScriptBuilder.new
+        c.instance_eval &block
+        @txout.pk_script = c.script
+      end
+
+    end
+    
   end
-
-  class TxInBuilder
-    attr_reader :key, :coinbase_data
-
-    def initialize
-      @txin = Bitcoin::P::TxIn.new
-    end
-
-    def prev_out tx
-      @prev_out = tx
-    end
-
-    def prev_out_index i
-      @prev_out_index = i
-    end
-
-    def sequence s
-      @sequence = s
-    end
-
-    def signature_key key
-      @key = key
-    end
-
-    def coinbase data = nil
-      @coinbase_data = data || OpenSSL::Random.random_bytes(32)
-      @prev_out = nil
-      @prev_out_index = 4294967295
-    end
-
-    def txin
-      @txin.prev_out = (@prev_out ? [@prev_out.hash].pack("H*").reverse : "\x00"*32)
-      @txin.prev_out_index = @prev_out_index
-      @txin.sequence = @sequence || "\xff\xff\xff\xff"
-      @txin
-    end
-  end
-
-  class ScriptBuilder
-    attr_reader :script
-
-    def initialize
-      @type = nil
-      @script = nil
-    end
-
-    def type type
-      @type = type.to_sym
-    end
-
-    def recipient data
-      @script = Bitcoin::Script.send("to_#{@type}_script", data)
-    end
-  end
-
-  class TxOutBuilder
-    attr_reader :txout
-
-    def initialize
-      @txout = Bitcoin::P::TxOut.new
-    end
-
-    def value value
-      @txout.value = value
-    end
-
-    def script &block
-      c = ScriptBuilder.new
-      c.instance_eval &block
-      @txout.pk_script = c.script
-    end
-
-  end
-
 end
