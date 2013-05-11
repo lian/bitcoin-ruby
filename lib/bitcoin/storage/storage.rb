ff74ebee9fe5148c62c3433457736a7cc407e26b
Marius Hanne <marius.hanne@sourceagency.org>
2013-05-11 02:16:46 +0200

    typo

--- lib/bitcoin/storage/storage.rb
+++ lib/bitcoin/storage/storage.rb
@@ -60,7 +60,7 @@ module Bitcoin::Storage
         res = store_block(blk)
         log.info { "block #{blk.hash} " +
           "[#{res[0]}, #{['main', 'side', 'orphan'][res[1]]}] " +
-          "(#{"%.4fs, %.3fkb" % [(Time.now - time), blk.payload.bytesize.to_f/1000]})" }  if res[1]
+          "(#{"%.4fs, %.3fkb" % [(Time.now - time), blk.payload.bytesize.to_f/1000]})" }  if res && res[1]
         res
       end
 
