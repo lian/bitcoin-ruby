d8663201b209448dd3a0d492ed4a77ea1ef20791
Marius Hanne <marius.hanne@sourceagency.org>
2014-02-23 07:57:53 +0100

    postgres store: change tx.hash column type to bytea

---
+++ lib/bitcoin/storage/sequel/migrations/005_change_tx_hash_to_bytea.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+
+  up do
+
+    @log.info { "Running migration #{__FILE__}" }
+
+    if adapter_scheme == :postgres
+      execute "ALTER TABLE tx ALTER COLUMN hash TYPE bytea USING hash::bytea"
+    end
+
+  end
+
+end
