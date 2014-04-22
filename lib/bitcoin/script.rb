34ad2ff5fbcb8096bc044bf9039984ca8f7065ac
mikem <mikem@tarix.net>
2014-04-22 12:32:51 +0900

    fix exception getting script address from invalid transactions (see testnet aba0441c4c9933dcd7db789c39053739ec435ab742ed2c23c05f22f1488c0bfd)

--- lib/bitcoin/script.rb
+++ lib/bitcoin/script.rb
@@ -486,7 +486,7 @@ class Bitcoin::Script
   # is this a pubkey script
   def is_pubkey?
     return false if @chunks.size != 2
-    (@chunks[1] == OP_CHECKSIG) && @chunks[0] && @chunks[0] != OP_RETURN
+    (@chunks[1] == OP_CHECKSIG) && @chunks[0] && (@chunks[0].is_a?(String)) && @chunks[0] != OP_RETURN
   end
   alias :is_send_to_ip? :is_pubkey?
 
