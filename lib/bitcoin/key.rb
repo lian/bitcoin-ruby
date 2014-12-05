c8d4522a5073343dd2eca544e93034469a12591d
Ross Nicoll <jrn@jrn.me.uk>
2014-12-05 19:41:56 +0000

    Modified transaction signing to always generate low-S value DER signatures.

--- lib/bitcoin/key.rb
+++ lib/bitcoin/key.rb
@@ -98,7 +98,12 @@ module Bitcoin
     #  key1 = Bitcoin::Key.generate
     #  sig = key1.sign("some data")
     def sign(data)
-      @key.dsa_sign_asn1(data)
+      sig = @key.dsa_sign_asn1(data)
+      if Script::is_low_der_signature?(sig)
+        sig
+      else
+        Bitcoin::OpenSSL_EC.signature_to_low_s(sig)
+      end
     end
 
     # Verify signature +sig+ for +data+.
