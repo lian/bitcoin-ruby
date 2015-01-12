a62a6c6d188210121074b7198d4bbbf066dd23ae
Julian Langschaedel <meta.rb@gmail.com>
2015-01-12 22:34:30 +0100

    update and cleanup libsecp256k1 functions

--- lib/bitcoin/ffi/secp256k1.rb
+++ lib/bitcoin/ffi/secp256k1.rb
@@ -1,28 +1,48 @@
 # encoding: ascii-8bit
 
 # Wraps libsecp256k1 (https://github.com/bitcoin/secp256k1)
+# commit: 50cc6ab0625efda6dddf1dc86c1e2671f069b0d8
 
 Bitcoin.require_dependency :ffi, exit: false, message: "Skipping FFI needed for libsecp256k1 methods."
 
 module Bitcoin
   module Secp256k1
     extend FFI::Library
-    ffi_lib 'secp256k1'
 
     SECP256K1_START_VERIFY = (1 << 0)
     SECP256K1_START_SIGN   = (1 << 1)
 
-    attach_function :secp256k1_start, [:int], :void
-    attach_function :secp256k1_stop, [], :void
-    attach_function :secp256k1_ec_seckey_verify, [:pointer], :int
-    attach_function :secp256k1_ec_pubkey_create, [:pointer, :pointer, :pointer, :int], :int
-    attach_function :secp256k1_ecdsa_sign, [:pointer, :int, :pointer, :pointer, :pointer, :pointer], :int
-    attach_function :secp256k1_ecdsa_verify, [:pointer, :int, :pointer, :int, :pointer, :int], :int
-    attach_function :secp256k1_ecdsa_sign_compact, [:pointer, :int, :pointer, :pointer, :pointer, :pointer], :int
-    attach_function :secp256k1_ecdsa_recover_compact, [:pointer, :int, :pointer, :pointer, :pointer, :int, :int], :int
+    def self.ffi_load_functions(file)
+      class_eval <<-RUBY
+        ffi_lib [ %[#{file}] ]
+
+        attach_function :secp256k1_start, [:int], :void
+        attach_function :secp256k1_stop, [], :void
+        attach_function :secp256k1_ec_seckey_verify, [:pointer], :int
+        attach_function :secp256k1_ec_pubkey_verify, [:pointer, :int], :int
+        attach_function :secp256k1_ec_pubkey_create, [:pointer, :pointer, :pointer, :int], :int
+
+        # int secp256k1_ecdsa_sign(const unsigned char *msg32, unsigned char *sig, int *siglen, const unsigned char *seckey, secp256k1_nonce_function_t noncefp, const void *ndata)
+        attach_function :secp256k1_ecdsa_sign, [:pointer, :pointer, :pointer, :pointer, :pointer, :pointer], :int
+
+        # int secp256k1_ecdsa_verify(const unsigned char *msg32, const unsigned char *sig, int siglen, const unsigned char *pubkey, int pubkeylen)
+        attach_function :secp256k1_ecdsa_verify, [:pointer, :pointer, :int, :pointer, :int], :int
+
+        # int secp256k1_ecdsa_sign_compact(const unsigned char *msg32, unsigned char *sig64, const unsigned char *seckey, secp256k1_nonce_function_t noncefp, const void *ndata, int *recid)
+        attach_function :secp256k1_ecdsa_sign_compact, [:pointer, :pointer, :pointer, :pointer, :pointer, :pointer], :int
+
+        # int secp256k1_ecdsa_recover_compact(const unsigned char *msg32, const unsigned char *sig64, unsigned char *pubkey, int *pubkeylen, int compressed, int recid)
+        attach_function :secp256k1_ecdsa_recover_compact, [:pointer, :pointer, :pointer, :pointer, :int, :int], :int
+      RUBY
+    end
 
     def self.init
       return if @secp256k1_started
+
+      lib_path = [ ENV['SECP256K1_LIB_PATH'], 'vendor/secp256k1/.libs/libsecp256k1.so' ].find{|f| File.exists?(f.to_s) }
+      lib_path = 'libsecp256k1.so' unless lib_path
+      ffi_load_functions(lib_path)
+
       secp256k1_start(SECP256K1_START_VERIFY | SECP256K1_START_SIGN)
       @secp256k1_started = true
     end
@@ -32,8 +52,7 @@ module Bitcoin
 
       while true do
         priv_key = SecureRandom.random_bytes(32)
-        priv_key_buf = FFI::MemoryPointer.new(:uchar, 32)
-        priv_key_buf.put_bytes(0, priv_key)
+        priv_key_buf = FFI::MemoryPointer.new(:uchar, 32).put_bytes(0, priv_key)
         break if secp256k1_ec_seckey_verify(priv_key_buf)
       end
 
@@ -48,70 +67,53 @@ module Bitcoin
     def self.sign(data, priv_key)
       init
 
-      data_buf = FFI::MemoryPointer.new(:uchar, data.bytesize)
-      data_buf.put_bytes(0, data)
+      msg32 = FFI::MemoryPointer.new(:uchar, 32).put_bytes(0, data)
+      seckey = FFI::MemoryPointer.new(:uchar, priv_key.bytesize).put_bytes(0, priv_key)
+      raise "priv_key invalid" unless secp256k1_ec_seckey_verify(seckey)
 
-      sig_buf = FFI::MemoryPointer.new(:uchar, 72)
-      sig_size = FFI::MemoryPointer.new(:int)
-      sig_size.write_int(72)
-
-      priv_key_buf = FFI::MemoryPointer.new(:uchar, priv_key.bytesize)
-      priv_key_buf.put_bytes(0, priv_key)
+      sig, siglen = FFI::MemoryPointer.new(:uchar, 72), FFI::MemoryPointer.new(:int).write_int(72)
 
       while true do
-        nonce = FFI::MemoryPointer.new(:uchar, 32)
-        nonce.put_bytes(0, SecureRandom.random_bytes(32))
-        break if secp256k1_ecdsa_sign(data_buf, data.bytesize, sig_buf, sig_size, priv_key, nonce)
+        break if secp256k1_ecdsa_sign(msg32, sig, siglen, seckey, nil, nil)
       end
 
-      sig_buf.read_string(sig_size.read_int)
+      sig.read_string(siglen.read_int)
     end
 
     def self.verify(data, signature, pub_key)
       init
 
-      data_buf = FFI::MemoryPointer.new(:uchar, data.bytesize)
-      data_buf.put_bytes(0, data)
+      data_buf = FFI::MemoryPointer.new(:uchar, 32).put_bytes(0, data)
+      sig_buf  = FFI::MemoryPointer.new(:uchar, signature.bytesize).put_bytes(0, signature)
+      pub_buf  = FFI::MemoryPointer.new(:uchar, pub_key.bytesize).put_bytes(0, pub_key)
 
-      sig_buf = FFI::MemoryPointer.new(:uchar, signature.bytesize)
-      sig_buf.put_bytes(0, signature)
+      result = secp256k1_ecdsa_verify(data_buf, sig_buf, sig_buf.size, pub_buf, pub_buf.size)
 
-      pub_key_buf = FFI::MemoryPointer.new(:uchar, pub_key.bytesize)
-      pub_key_buf.put_bytes(0, pub_key)
-
-      result = secp256k1_ecdsa_verify(data_buf, data.bytesize,
-                                      sig_buf, signature.bytesize,
-                                      pub_key_buf, pub_key.bytesize)
-      if result == -1
-        raise "error invalid pubkey"
-      elsif result == -2
-        raise "error invalid signature"
+      case result
+      when  0; false
+      when  1; true
+      when -1; raise "error invalid pubkey"
+      when -2; raise "error invalid signature"
+      else   ; raise "error invalid result"
       end
-
-      result == 1
     end
 
     def self.sign_compact(message, priv_key, compressed=true)
       init
 
-      message_buf = FFI::MemoryPointer.new(:uchar, message.bytesize)
-      message_buf.put_bytes(0, message)
-
-      sig_buf = FFI::MemoryPointer.new(:uchar, 64)
-
-      priv_key_buf = FFI::MemoryPointer.new(:uchar, priv_key.bytesize)
-      priv_key_buf.put_bytes(0, priv_key)
-
+      msg32 = FFI::MemoryPointer.new(:uchar, 32).put_bytes(0, message)
+      sig64 = FFI::MemoryPointer.new(:uchar, 64)
       rec_id = FFI::MemoryPointer.new(:int)
 
+      seckey = FFI::MemoryPointer.new(:uchar, priv_key.bytesize).put_bytes(0, priv_key)
+      raise "priv_key invalid" unless secp256k1_ec_seckey_verify(seckey)
+
       while true do
-        nonce = FFI::MemoryPointer.new(:uchar, 32)
-        nonce.put_bytes(0, SecureRandom.random_bytes(32))
-        break if secp256k1_ecdsa_sign_compact(message_buf, message.bytesize, sig_buf, priv_key, nonce, rec_id)
+        break if secp256k1_ecdsa_sign_compact(msg32, sig64, seckey, nil, nil, rec_id)
       end
 
       header = [27 + rec_id.read_int + (compressed ? 4 : 0)].pack("C")
-      [ header, sig_buf.read_string(64) ].join
+      [ header, sig64.read_string(64) ].join
     end
 
     def self.recover_compact(message, signature)
@@ -124,28 +126,18 @@ module Bitcoin
 
       compressed = version >= 31 ? true : false
       version -= 4 if compressed
-      rec_id = version - 27
 
-      message_buf = FFI::MemoryPointer.new(:uchar, message.bytesize)
-      message_buf.put_bytes(0, message)
+      recid = version - 27
+      msg32 = FFI::MemoryPointer.new(:uchar, 32).put_bytes(0, message)
+      sig64 = FFI::MemoryPointer.new(:uchar, 64).put_bytes(0, signature[1..-1])
+      pubkey = FFI::MemoryPointer.new(:uchar, pub_key_len = compressed ? 33 : 65)
+      pubkeylen = FFI::MemoryPointer.new(:int).write_int(pub_key_len)
 
-      signature[0] = ''
-      sig_buf = FFI::MemoryPointer.new(:uchar, signature.bytesize)
-      sig_buf.put_bytes(0, signature)
+      result = secp256k1_ecdsa_recover_compact(msg32, sig64, pubkey, pubkeylen, (compressed ? 1 : 0), recid)
 
-      pub_key_len = compressed ? 33 : 65
-      pub_key_buf = FFI::MemoryPointer.new(:uchar, pub_key_len)
-      pub_key_size = FFI::MemoryPointer.new(:int)
-      pub_key_size.write_int(pub_key_len)
-
-      result = secp256k1_ecdsa_recover_compact(message_buf, message.bytesize,
-                                               sig_buf,
-                                               pub_key_buf, pub_key_size,
-                                               compressed ? 1 : 0,
-                                               rec_id)
       return nil unless result
 
-      pub_key_buf.read_bytes(pub_key_size.read_int)
+      pubkey.read_bytes(pubkeylen.read_int)
     end
 
   end
--- spec/bitcoin/secp256k1_spec.rb
+++ spec/bitcoin/secp256k1_spec.rb
@@ -5,6 +5,16 @@ require 'bitcoin'
 
 describe 'libsecp256k1' do
 
+  it 'generate key pair' do
+    priv, pub = Bitcoin::Secp256k1.generate_key_pair(compressed = true)
+    [priv, pub].map(&:bytesize).should == [32, 33]
+    ["\x03", "\x02"].include?(pub[0]).should == true
+
+    priv, pub = Bitcoin::Secp256k1.generate_key_pair(compressed = false)
+    [priv, pub].map(&:bytesize).should == [32, 65]
+    ["\x04"].include?(pub[0]).should == true
+  end
+
   it 'sign and verify' do
     priv, pub = Bitcoin::Secp256k1.generate_key_pair
     signature = Bitcoin::Secp256k1.sign("derp", priv)
@@ -12,8 +22,8 @@ describe 'libsecp256k1' do
   end
 
   it 'sign compact and recover' do
-    priv, pub = Bitcoin::Secp256k1.generate_key_pair
-    signature = Bitcoin::Secp256k1.sign_compact("derp", priv)
+    priv, pub = Bitcoin::Secp256k1.generate_key_pair(compressed=true)
+    signature = Bitcoin::Secp256k1.sign_compact("derp", priv, compressed=true)
     signature.bytesize.should == 65
     pub2 = Bitcoin::Secp256k1.recover_compact("derp", signature)
     pub2.bytesize.should == 33
@@ -26,6 +36,13 @@ describe 'libsecp256k1' do
     pub2 = Bitcoin::Secp256k1.recover_compact("derp", signature)
     pub2.bytesize.should == 65
     pub2.should == pub
+  end
+
+  it 'deterministic signature using rfc6979' do
+    priv, pub = Bitcoin::Secp256k1.generate_key_pair
+    first  = Bitcoin::Secp256k1.sign("derp", priv)
+    second = Bitcoin::Secp256k1.sign("derp", priv)
+    first.should == second
   end
 
 end
