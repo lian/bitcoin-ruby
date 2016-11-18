61ca8f51fbf962f9abb67a18abf38529dbd21cd3
azuchi <azuchi@haw.co.jp>
2016-11-18 11:48:50 +0900

    Add parse and payload logic for witness tx

---
+++ lib/bitcoin/protocol/tx_witness.rb
@@ -0,0 +1,31 @@
+# encoding: ascii-8bit
+
+module Bitcoin
+  module Protocol
+
+    class TxWitness
+
+      #
+      attr_reader :tx_in_wit
+
+      def initialize
+        @tx_in_wit = []
+      end
+
+      # Add witness
+      # @param[Bitcoin::Protocol::TxInWitness] tx_in_wit witness object
+      def add_witness(tx_in_wit)
+        (@tx_in_wit ||= []) << tx_in_wit
+      end
+
+      # output witness in raw binary format
+      def to_payload
+        payload = ""
+        @tx_in_wit.each{|w|payload << w.to_payload}
+        payload
+      end
+
+    end
+
+  end
+end
