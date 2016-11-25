95dcd3c5734df4986b7b2c301f27b7a311d06423
azuchi <azuchi@haw.co.jp>
2016-11-25 15:05:09 +0900

    Add witness script generation

--- spec/bitcoin/script/script_spec.rb
+++ spec/bitcoin/script/script_spec.rb
@@ -377,6 +377,17 @@ describe 'Bitcoin::Script' do
         Script.from_string("OP_HASH160 #{hash160} OP_EQUAL").raw
     end
 
+    it "to_witness_hash160_script" do
+      hash160 = Bitcoin.hash160('025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee6357')
+      Script.to_witness_hash160_script(hash160).should == Script.from_string("0 #{hash160}").raw
+    end
+
+    it "should generate p2wsh script" do
+      witness_script = '21026dccc749adc2a9d0d89497ac511f760f45c47dc5ed9cf352a58ac706453880aeadab210255a9626aebf5e29c0e6538428ba0d1dcf6ca98ffdf086aa8ced5e0d0215ea465ac'
+      sha256 = Bitcoin.sha256(witness_script)
+      Script.to_witness_p2sh_script(sha256).should == Script.from_string("0 #{sha256}").raw
+    end
+
     it "should generate op_return script" do
       Script.to_op_return_script("deadbeef").should == SCRIPT[6]
       Script.to_op_return_script.should == Script.from_string("OP_RETURN").raw
