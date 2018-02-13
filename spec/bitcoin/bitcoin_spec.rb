lian/bitcoin-ruby

commit 6413cd1bd0aff4521b4252cdf8973b10102cf003

Litecoin P2SH prefix

lib/bitcoin.rb
@@ -657,7 +657,7 @@ module Bitcoin
       magic_head: "\xfb\xc0\xb6\xdb",
       message_magic: "Litecoin Signed Message:\n",
       address_version: "30",
-      p2sh_version: "05",
+      p2sh_version: "32",
       privkey_version: "b0",
       extended_privkey_version: "019d9cfe",
       extended_pubkey_version: "019da462",
@@ -751,7 +751,7 @@ module Bitcoin
       retarget_time_new: 60, # 1 minute
       target_spacing: 60, # block interval
       dns_seeds: [
-        "seed.dogechain.info", 
+        "seed.dogechain.info",
         "seed.dogecoin.com",
       ],
       genesis_hash: "1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691",
@@ -809,7 +809,7 @@ module Bitcoin
       retarget_time_new: 60, # 1 minute
       target_spacing: 60, # block interval
       max_money: 100_000_000_000 * COIN,
-      dns_seeds: [ 
+      dns_seeds: [
         "testdoge-seed.lionservers.de",
       ],
       genesis_hash: "bb0a78264637406b6360aad926284d544d7049f45189db5664f3c4d07350559e",
@@ -817,7 +817,7 @@ module Bitcoin
       alert_pubkeys: [],
       known_nodes: [
         "localhost",
-        "testnets.chain.so", 
+        "testnets.chain.so",
       ],
       checkpoints: {
         546 => "ac537cfeda975e45040e9938d08e40a16e0fbd6388d02d9b4928b8ae0108c626",

commit caa502d21eba7713fa5f58315442d3336a8b5a8a

Recognize legacy Litecoin P2SH addresses as valid.
Commit 6413cd1bd0aff4521b4252cdf8973b10102cf003 caused an issue where
bitcoin-ruby does not detect legacy Litecoin addesses as valid. Even
though the version byte is upgraded, this library should recognize
both for backwards-compatibility.

lib/bitcoin.rb
@@ -99,10 +99,16 @@ module Bitcoin
 
       hex = decode_base58(address) rescue nil
       if hex && hex.bytesize == 50 && address_checksum?(address)
+        # Litecoin updates the P2SH version byte, and this method should recognize both.
+        p2sh_versions = [p2sh_version]
+        if Bitcoin.network[:legacy_p2sh_versions]
+          p2sh_versions += Bitcoin.network[:legacy_p2sh_versions]
+        end
+
         case hex[0...2]
         when address_version
           return :hash160
-        when p2sh_version
+        when *p2sh_versions
           return :p2sh
         end
       end
@@ -727,6 +733,7 @@ module Bitcoin
       message_magic: "Litecoin Signed Message:\n",
       address_version: "30",
       p2sh_version: "32",
+      legacy_p2sh_versions: ["05"],
       privkey_version: "b0",
       bech32_hrp: "ltc",
       extended_privkey_version: "019d9cfe",
@@ -778,6 +785,7 @@ module Bitcoin
       magic_head: "\xfd\xd2\xc8\xf1",
       address_version: "6f",
       p2sh_version: "3a",
+      legacy_p2sh_versions: nil,
       privkey_version: "ef",
       bech32_hrp: "tltc",
       extended_privkey_version: "0436ef7d",

spec/bitcoin/bitcoin_spec.rb
@@ -192,6 +192,14 @@ describe 'Bitcoin Address/Hash160/PubKey' do
     Bitcoin.address_type("bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4").should == :witness_v0_keyhash
     Bitcoin.address_type("bc1qw508d6qejxtdg4y5r3zarvayr0c5xw7kv8f3t4").should == nil
     Bitcoin.address_type("bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3").should == :witness_v0_scripthash
+
+    # address_type recognizes Litecoin addresses encoded with the legacy P2SH version byte
+    Bitcoin.network = :litecoin
+    Bitcoin.address_type("3CkxTG25waxsmd13FFgRChPuGYba3ar36B").should == :p2sh
+    Bitcoin.address_type("MJy6m9S3thpJa8GwM8fm2LeJbFC22w18Vx").should == :p2sh
+    Bitcoin.address_type("2MyLngQnhzjzatKsB7XfHYoP9e2XUXSiBMM").should == nil
+
+    Bitcoin.network = :bitcoin
   end
 
   it 'Bitcoin#checksum' do