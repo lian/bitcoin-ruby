4b9e8b233b33714dd56c6899a1a0f828ef26b241
Eli Haims <elihaims@gmail.com>
2018-08-07 13:10:35 -0700

    Fix rubocop offenses for connection

--- lib/bitcoin/connection.rb
+++ lib/bitcoin/connection.rb
@@ -6,7 +6,7 @@ require 'bitcoin'
 require 'resolv'
 
 module Bitcoin
-
+  # Handle messages received from node
   module ConnectionHandler
     def on_inv_transaction(hash)
       p ['inv transaction', hash.hth]
@@ -38,13 +38,13 @@ module Bitcoin
 
     def on_block(block)
       p ['block', block.hash]
-      #p block.payload.each_byte.map{|i| "%02x" % [i] }.join(" ")
-      #puts block.to_json
+      # p block.payload.each_byte.map{|i| "%02x" % [i] }.join(" ")
+      # puts block.to_json
     end
 
     def on_version(version)
       p [@sockaddr, 'version', version, version.time - Time.now.to_i]
-      send_data( Protocol.verack_pkt )
+      send_data(Protocol.verack_pkt)
     end
 
     def on_verack
@@ -59,17 +59,17 @@ module Bitcoin
     end
 
     def query_blocks
-      start = ("\x00"*32)
-      stop  = ("\x00"*32)
-      pkt = Protocol.pkt("getblocks", "\x00" + start + stop )
+      start = ("\x00" * 32)
+      stop  = ("\x00" * 32)
+      pkt = Protocol.pkt('getblocks', "\x00" + start + stop)
       send_data(pkt)
     end
 
     def on_handshake_begin
-      block   = 127953
-      from    = "127.0.0.1:8333"
+      block   = 127_953
+      from    = '127.0.0.1:8333'
       from_id = Bitcoin::Protocol::Uniq
-      to      = @sockaddr.reverse.join(":")
+      to      = @sockaddr.reverse.join(':')
       # p "==", from_id, from, to, block
       pkt = Protocol.version_pkt(from_id, from, to, block)
       p ['sending version pkt', pkt]
@@ -77,19 +77,19 @@ module Bitcoin
     end
   end
 
-
+  # Establish connection to node
   class Connection < EM::Connection
     include ConnectionHandler
 
     def initialize(host, port, connections)
       @sockaddr = [port, host]
       @connections = connections
-      @parser = Bitcoin::Protocol::Parser.new( self )
+      @parser = Bitcoin::Protocol::Parser.new(self)
     end
 
     def post_init
       p ['connected', @sockaddr]
-      EM.schedule{ on_handshake_begin }
+      EM.schedule { on_handshake_begin }
     end
 
     def receive_data(data)
@@ -107,24 +107,21 @@ module Bitcoin
 
     def self.connect_random_from_dns(connections)
       seeds = Bitcoin.network[:dns_seeds]
-      if seeds.any?
-        host = Resolv::DNS.new.getaddresses(seeds.sample).map {|a| a.to_s}.sample
-        connect(host, Bitcoin::network[:default_port], connections)
-      else
-        raise "No DNS seeds available. Provide IP, configure seeds, or use different network."
+      if seeds.empty?
+        raise 'No DNS seeds available. Provide IP, configure seeds, or use different network.'
       end
+
+      host = Resolv::DNS.new.getaddresses(seeds.sample).map(&:to_s).sample
+      connect(host, Bitcoin.network[:default_port], connections)
     end
   end
 end
 
-
-if $0 == __FILE__
+if $PROGRAM_NAME == __FILE__
   EM.run do
-
     connections = []
-    #Bitcoin::Connection.connect('127.0.0.1', 8333, connections)
-    #Bitcoin::Connection.connect('217.157.1.202', 8333, connections)
+    # Bitcoin::Connection.connect('127.0.0.1', 8333, connections)
+    # Bitcoin::Connection.connect('217.157.1.202', 8333, connections)
     Bitcoin::Connection.connect_random_from_dns(connections)
-
   end
 end
